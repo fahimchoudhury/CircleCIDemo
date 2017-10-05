54c923f9652cc779b1eec5eac4a76b22be32c68f
Fahim Masud Choudhury <fahim.choudhury@sekai-lab-bd.com>
2017-10-05 16:18:50 +0600

    Add Deploygate integration to CircleCI
    Deploy only on release branch

--- script/setup_deploygate.sh
+++ script/setup_deploygate.sh
@@ -1,7 +1,24 @@
 #!/usr/bin/env bash
-./gradlew assembleRelease
 
-APKPATH="app/build/outputs/apk/*release*.apk"
+if [ "$CIRCLE_BRANCH" == "release" ]; then
+  echo "Currently on release branch"
+  echo "Creating signed release apk..."
+  ./gradlew assembleRelease
 
-curl -F "file=@sample.apk" -F "token=$ENV_DEPLOYGATE_API" -F "message=Testing CircleCI integration with Deploygate" https://deploygate.com/api/users/fahimchoudhury/apps
+  RELEASE_APK="app/build/outputs/apk/*release*.apk"
+
+  if [ -f $RELEASE_APK ]; then
+    echo "Release APK found"
+    echo "Uploading to Deploygate..."
+    curl -F "file=@$RELEASE_APK" -F "token=$ENV_DEPLOYGATE_API" -F "message=Testing CircleCI integration with Deploygate" https://deploygate.com/api/users/$ENV_DEPLOYGATE_USER/apps
+  else
+    echo "Release APK not found, aborting..."
+  fi
+else
+  echo "Not release branch so not deploying"
+fi
+
+
+
+
 
