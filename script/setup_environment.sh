085580ed2329a667d581897c97fda78e7b011a75
Fahim Masud Choudhury <fahim.choudhury@sekai-lab-bd.com>
2017-10-05 12:29:04 +0600

    Add release APK generation

--- script/setup_environment.sh
+++ script/setup_environment.sh
@@ -3,10 +3,8 @@
 
 function decrypt_keystore {
 ENCRYPTED_KEYSTORE="keystore.enc"
-export ENCRYPTED_KEYSTORE
 
-KEYSTORE_FILE=$CIRCLE_WORKING_DIRECTORY"/circleci-demo-keystore.jks"
-export KEYSTORE_FILE
+KEYSTORE_FILE="$CIRCLE_WORKING_DIRECTORY/circleci-demo-keystore.jks"
 
 echo "circleci-demo-keystore.jks should exist at $KEYSTORE_FILE"
 
@@ -18,25 +16,23 @@ fi
 }
 
 function setup_signing_config {
-KEYSTORE_FILE=$CIRCLE_WORKING_DIRECTORY"/circleci-demo-keystore.jks"
-export KEYSTORE_FILE
+KEYSTORE_FILE="$CIRCLE_WORKING_DIRECTORY/circleci-demo-keystore.jks"
 
-KEYSTORE_PROPERTIES=$CIRCLE_WORKING_DIRECTORY"/keystore.properties"
-export KEYSTORE_PROPERTIES
+KEYSTORE_PROPERTIES="$CIRCLE_WORKING_DIRECTORY/keystore.properties"
 
 echo "keystore.properties should exist at $KEYSTORE_PROPERTIES"
 
 if [ ! -f "$KEYSTORE_PROPERTIES" ]; then
   echo "keystore.properties doesn't exist"
 
-  echo "Creating keystore.properties file..."
-  touch $KEYSTORE_PROPERTIES
+  echo "Creating keystore.properties file in project root directory..."
+  touch keystore.properties
 
   echo "Writing signingConfigs for release builds to keystore.properties..."
-  echo "storeFile=$KEYSTORE_FILE" >> $KEYSTORE_PROPERTIES
-  echo "storePassword=$ENV_STORE_PASSWORD" >> $KEYSTORE_PROPERTIES
-  echo "keyAlias=$ENV_KEY_ALIAS" >> $KEYSTORE_PROPERTIES
-  echo "keyPassword=$ENV_KEY_PASSWORD" >> $KEYSTORE_PROPERTIES
+  echo "storeFile=$KEYSTORE_FILE" >> "$KEYSTORE_PROPERTIES"
+  echo "storePassword=$ENV_STORE_PASSWORD" >> "$KEYSTORE_PROPERTIES"
+  echo "keyAlias=$ENV_KEY_ALIAS" >> "$KEYSTORE_PROPERTIES"
+  echo "keyPassword=$ENV_KEY_PASSWORD" >> "$KEYSTORE_PROPERTIES"
 
 fi
 }
